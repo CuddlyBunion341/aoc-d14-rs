b8be7bd5d962000166520feeb1651654d9d5bb5f
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-14 21:09:22 +0100

    First steps

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,55 @@
 use std::env;
 use std::fs;
 
+#[derive(Debug)]
+struct Vec2 {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Debug)]
+struct Robot {
+    position: Vec2,
+    velocity: Vec2,
+}
+
+fn extract_value(string: &str) -> Vec2 {
+    let split: Vec<&str> = string.split(",").collect();
+
+    let x_part = split.get(0).unwrap();
+    let y_part = split.get(1).unwrap();
+
+    let x_string_parts: Vec<&str> = x_part.split("=").collect();
+    let x_part = x_string_parts.get(1).unwrap();
+
+    let x: i32 = x_part.parse().unwrap();
+    let y: i32 = y_part.parse().unwrap();
+
+    Vec2 {
+        x,
+        y
+    }
+}
+
 fn main() {
     let contents = fs::read_to_string("./input")
         .expect("Should have been able to read the file");
-    println!("With text:\n{contents}");
+    println!("Parsed input:\n{contents}");
+
+    let lines = contents.split("\n");
+
+    lines.into_iter().for_each(|line| {
+        if line == "" {
+            return;
+        }
+
+        let split: Vec<&str> = line.split(" ").collect();
+        let pos = extract_value(split.get(0).unwrap());
+        let vel = extract_value(split.get(1).unwrap());
+
+        let robot = Robot {
+            position: pos,
+            velocity: vel
+        };
+    });
 }
