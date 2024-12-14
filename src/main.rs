44033ef89636f293f93347281d0e4d1e75c4fbd7
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-14 21:12:34 +0100

    Step 2

--- src/main.rs
+++ src/main.rs
@@ -25,31 +25,44 @@ fn extract_value(string: &str) -> Vec2 {
     let x: i32 = x_part.parse().unwrap();
     let y: i32 = y_part.parse().unwrap();
 
-    Vec2 {
-        x,
-        y
+    Vec2 { x, y }
+}
+
+fn tick_robot_system(robot: Robot) -> Robot {
+    Robot {
+        position: Vec2 {
+            x: robot.position.x,
+            y: robot.position.y,
+        },
+        velocity: Vec2 {
+            x: robot.velocity.x,
+            y: robot.velocity.y,
+        },
     }
 }
 
+fn extract_robot_from_line(line: &str) -> Option<Robot> {
+    if line == "" {
+        return None;
+    }
+
+    let split: Vec<&str> = line.split(" ").collect();
+    let pos = extract_value(split.get(0).unwrap());
+    let vel = extract_value(split.get(1).unwrap());
+
+    let robot = Robot {
+        position: pos,
+        velocity: vel,
+    };
+
+    Some(robot)
+}
+
 fn main() {
-    let contents = fs::read_to_string("./input")
-        .expect("Should have been able to read the file");
+    let contents = fs::read_to_string("./input").expect("Should have been able to read the file");
     println!("Parsed input:\n{contents}");
 
     let lines = contents.split("\n");
 
-    lines.into_iter().for_each(|line| {
-        if line == "" {
-            return;
-        }
-
-        let split: Vec<&str> = line.split(" ").collect();
-        let pos = extract_value(split.get(0).unwrap());
-        let vel = extract_value(split.get(1).unwrap());
-
-        let robot = Robot {
-            position: pos,
-            velocity: vel
-        };
-    });
+    lines.into_iter().for_each(|line| {});
 }
