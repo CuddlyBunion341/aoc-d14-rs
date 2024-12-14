32df8d4cfd80f5466e9895d159223bd0ceae15bd
Daniel Bengl <daniel.bengl@renuo.ch>
2024-12-14 20:53:34 +0100

    Add input reading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let contents = fs::read_to_string("./input")
+        .expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
 }
